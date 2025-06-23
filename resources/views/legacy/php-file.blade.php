b43a4fe3ed6fdea73c3bdd5a9484957f3b8ca4f0
German Fernandez <german@ultimatejetvacations.com>
2025-06-23 20:46:05 -0300

    commit ini

--- resources/views/legacy/php-file.blade.php
+++ resources/views/legacy/php-file.blade.php
@@ -1,6 +1,9 @@
 @php
     // Create an instance of clsIndicePaginas for all legacy endpoints
     include_once(config('legacy.get_directorio') . 'clsIndicePaginas.php');
+    include_once(config('legacy.get_directorio').'clsIni.php');
+
+    $objIni = new clsIni();
     $objIndice = new clsIndicePaginas();
 
     // Access the filePath variable passed from the controller
