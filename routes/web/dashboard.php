83d9ce27668809a7ec7e35944e4c6fbd1bcbab2a
german <fernandezgermandario@gmail.com>
2025-09-12 13:23:34 -0300

    fixes

---
+++ routes/web/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\ApiResources\ApiResourceBase;
+
+// GET /api/{entity}/{id}
+
+
+Route::get('/d', [\App\Http\Controllers\Dashboard\DashboardController::class, 'index'])->where('id', '[0-9]+');
+
+
+
