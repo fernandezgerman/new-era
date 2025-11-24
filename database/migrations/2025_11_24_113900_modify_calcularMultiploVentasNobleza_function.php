b4bcb29fc270ee4b6429c8975ee04cc3434625a4
german <fernandezgermandario@gmail.com>
2025-11-24 12:04:52 -0300

    update ventas de nobleza

---
+++ database/migrations/2025_11_24_113900_modify_calcularMultiploVentasNobleza_function.php
@@ -0,0 +1,86 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        DB::unprepared('DROP FUNCTION IF EXISTS `calcularMultiploVentasNobleza`');
+
+        DB::unprepared("
+CREATE  FUNCTION `calcularMultiploVentasNobleza`(
+    p_articulo_id bigint(20),
+    p_rubro_id bigint(20)
+        ) RETURNS decimal(15,2)
+    NO SQL
+    DETERMINISTIC
+BEGIN
+
+	declare v_multiplo decimal(15,2);
+	declare v_week  tinyint(4);
+
+	SET v_week = 42;
+
+	SET v_multiplo = 1;
+
+	IF(p_rubro_id = 94)THEN
+		SET v_multiplo = 0.1;
+	END IF;
+
+
+	if(p_rubro_id = 4) THEN
+		if(year(now()) =2025 )THEN
+			SET v_multiplo = 0.9;
+end if;
+end if;
+
+
+RETURN v_multiplo;
+END ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP FUNCTION IF EXISTS `calcularMultiploVentasNobleza`');
+
+        DB::unprepared("
+CREATE  FUNCTION `calcularMultiploVentasNobleza`(
+    p_articulo_id bigint(20),
+    p_rubro_id bigint(20)
+        ) RETURNS decimal(15,2)
+    NO SQL
+    DETERMINISTIC
+BEGIN
+
+	declare v_multiplo decimal(15,2);
+	declare v_week  tinyint(4);
+
+	SET v_week = 42;
+
+	SET v_multiplo = 1;
+
+	IF(p_rubro_id = 94)THEN
+		SET v_multiplo = 0.1;
+	END IF;
+
+
+	if(p_rubro_id = 4 and false) THEN
+		if(year(now()) =2025 )THEN
+			SET v_multiplo = 0.9;
+        end if;
+    end if;
+
+
+RETURN v_multiplo;
+END ");
+    }
+};
