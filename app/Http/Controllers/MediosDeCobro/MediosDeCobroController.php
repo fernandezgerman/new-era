f530d1deceb684a03dcecd0dc382a8390213f811
german <fernandezgermandario@gmail.com>
2025-12-03 19:38:43 -0300

    fixes & command to resend events

--- app/Http/Controllers/MediosDeCobro/MediosDeCobroController.php
+++ app/Http/Controllers/MediosDeCobro/MediosDeCobroController.php
@@ -83,7 +83,7 @@ class MediosDeCobroController extends BaseController
         $modosDeCobroManager->generarCobro($ventaSucursalCobro);
 
         return [
-            'link' => url('medios-de-pago/order/'.$ventaSucursalCobro->idunicolegacy.'/legacy-preview'),
+            'link' => env('APP_URL').'/medios-de-pago/order/'.$ventaSucursalCobro->idunicolegacy.'/legacy-preview',
             'ventaSucursalCobro' => $ventaSucursalCobro
         ];
     }
