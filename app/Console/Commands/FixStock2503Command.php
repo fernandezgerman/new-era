aa0e513db28bb30d80a810f9bf9e0064f9950ecf
german <fernandezgermandario@gmail.com>
2026-03-25 19:42:05 -0300

    fix stock 2503

--- app/Console/Commands/FixStock2503Command.php
+++ app/Console/Commands/FixStock2503Command.php
@@ -42,7 +42,7 @@ class FixStock2503Command extends Command
             })
             ->select('rbr.nombre as rubro_nombre', 'art.codigo', 'art.nombre as articulo_nombre', 'suc.nombre as sucursal_nombre', 'art.id as idarticulo', 'suc.id as idsucursal')
             ->whereBetween('r.fechaapertura', ['2026-03-24 06:43:29', '2026-03-25 09:36:39'])
-            ->groupBy('suc.nombre', 'art.nombre', 'art.id', 'suc.id', 'art.codigo')
+            ->groupBy('rbr.nombre','suc.nombre', 'art.nombre', 'art.id', 'suc.id', 'art.codigo')
             ->orderBy('suc.nombre', 'asc')
             ->orderBy('art.nombre', 'desc');
 
