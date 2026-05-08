d20d484cb0391b78ce34c3e6cfbfb9b9de9ae0e3
german <fernandezgermandario@gmail.com>
2026-05-08 13:07:48 -0300

    integraciones API

--- routes/api/integraciones.php
+++ routes/api/integraciones.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Integraciones\IntegracionesArticulosController;
+use App\Http\Middleware\CheckLegacyPermissions;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('integraciones')->middleware('auth.integraciones')->group(function () {
+Route::prefix('integraciones')->middleware('auth.integraciones')
+    ->withoutMiddleware(['auth:sanctum', 'restrict.access.per.hour', CheckLegacyPermissions::class])
+    ->group(function () {
     Route::get('articulo/{codigo}', [IntegracionesArticulosController::class, 'showByCodigo']);
     Route::get('articulo/buscar/{descripcion}', [IntegracionesArticulosController::class, 'searchByDescripcion']);
 });
