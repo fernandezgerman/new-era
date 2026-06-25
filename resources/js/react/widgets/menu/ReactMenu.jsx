6a7999d5818615b2e645a111b56fcae9ab4acacf
german <fernandezgermandario@gmail.com>
2026-06-25 14:36:04 -0300

    reporte ed existencias sin compras

--- resources/js/react/widgets/menu/ReactMenu.jsx
+++ resources/js/react/widgets/menu/ReactMenu.jsx
@@ -5,7 +5,7 @@ import {AccesosAlSistema} from "@/pages/accesosAlSistema/index.jsx";
 import {ListarAudits} from "@/pages/audits/index.jsx";
 import {ValorizacionHistorico} from "@/pages/ValorizacionHistorico/index.jsx";
 import {ArreglosDeStockChartCard} from "@/widgets/Dashboard/ArreglosStockChart/index.jsx";
-//import {ProcesamientoDeCostosDocumentacion} from "@/pages/documentacion/procesamientoDeCostos/index.jsx";
+import {ProcesamientoDeCostosDocumentacion} from "@/pages/documentacion/procesamientoDeCostos/index.jsx";
 import {ReporteGastos} from "@/pages/reporteGastos/index.jsx";
 import {ProveedoresListasImportar} from "@/pages/proveedoresImportarListas/index.jsx";
 import {OrdenesDeCompraReporte} from "@/pages/ordenesDeCompra/index.jsx";
